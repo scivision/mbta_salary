1f0880b39ff88c38d91c0c98899c9c6407353e2b
Michael Hirsch, Ph.D <scivision@users.noreply.github.com>
2018-07-20 15:59:17 -0400

    template

---
+++ README.md
@@ -0,0 +1,162 @@
+# MBTA Salary Analysis
+
+Year 2015 data thanks to Matthew Rocheleau, the Boston Globe staff reporter on the 
+[22 Dec 2015 MBTA overtime article](http://www.bostonglobe.com/2015/12/21/mbta-employees-who-will-make-more-than-this-year/u6BUkDr6EawQ7dlHx9bZQP/story.html).
+
+1. extract text tables from PDF using Poppler `pdftotext`
+2. load with Pandas
+3. look for interesting aspects of the data.
+
+For example, in 2014, the top MBTA earner was a Technician with an
+estimated $145 K of overtime on top of an $89 K salary. Excessive
+overtime can be a concern for worker performance and safety in any
+field, with possible drastic consequences for the public when working in
+sensitive areas like transportation and medicine.
+
+The Washingtonian published a 9 Dec 2015
+[article](http://www.washingtonian.com/blogs/capitalcomment/transportation/why-does-metro-suck-dangerous-accidents-escalator-outages.php)
+by Luke Mullins and Michael Gaynor that explores at length issues with WMATA and overtime abuses.
+
+Note: I exclude Backpay as it represents a retroactive increase in base salary for the previous year. 
+Backpay is last year's pay, paid out this year.
+
+
+![MBTA salary histogram by category](plots/2015hist.png)
+
+![MBTA salary by category](plots/2015cat.png)
+
+## Examples
+
+We currently have 2013-2015 salary data. Other trend analyses may find
+it useful to request prior years data.
+
+### 2015 MBTA Salary
+
+./salary.py EMP-2015-Gross-Bpay-Ovt.xlsx 2015
+
+yields plots and the output:
+
+Highest earning MBTA staff $256080 in 2015 was a Foreperson,Section,
+including estimated $171257 overtime.
+
++--------+---------+---------+---------+---------+-------+
+| type   | max     | median  | 90th    | subtot  | num   |
++========+=========+=========+=========+=========+=======+
+| signal | 182049  | 117083  | 151384  | > 2e+07 | > 157 |
++--------+---------+---------+---------+---------+-------+
+| maint  | 256080  | > 97309 | 134881  | > 2e+08 | 1743  |
++--------+---------+---------+---------+---------+-------+
+| ops    | 145266  | > 96538 | 113948  | > 1e+07 | > 128 |
++--------+---------+---------+---------+---------+-------+
+| police | 203553  | > 93684 | 136959  | > 3e+07 | > 271 |
++--------+---------+---------+---------+---------+-------+
+| heavy  | 178438  | > 80498 | 103964  | > 5e+07 | > 678 |
++--------+---------+---------+---------+---------+-------+
+| bus-ft | 120281  | > 75542 | > 91287 | > 1e+08 | 1304  |
++--------+---------+---------+---------+---------+-------+
+| subway | 158950  | > 73563 | > 94860 | > 4e+07 | > 628 |
++--------+---------+---------+---------+---------+-------+
+| bus-pt | > 54738 | > 40934 | > 49124 | > 2e+07 | > 410 |
++--------+---------+---------+---------+---------+-------+
+| rest   | 231589  | > 87497 | 122225  | > 1e+08 | 1149  |
++--------+---------+---------+---------+---------+-------+
+| all    | 256080  | > 82319 | 118860  | > 5e+08 | 6468  |
++--------+---------+---------+---------+---------+-------+
+
+### 2014 MBTA Salary
+
+./salary.py Wages2014.txt 2014
+
+yields plots and the output:
+
+Highest earning MBTA staff $235194 in 2014 was a Technician,Power
+Equip, including estimated $145401 overtime.
+
++--------+---------+---------+---------+---------+-------+
+| type   | max     | median  | 90th    | subtot  | num   |
++========+=========+=========+=========+=========+=======+
+| signal | 179689  | 110775  | 127210  | > 2e+07 | > 155 |
++--------+---------+---------+---------+---------+-------+
+| police | 221910  | 107261  | 165080  | > 3e+07 | > 259 |
++--------+---------+---------+---------+---------+-------+
+| maint  | 235194  | > 95772 | 131344  | > 2e+08 | 1727  |
++--------+---------+---------+---------+---------+-------+
+| ops    | 138506  | > 93504 | 115208  | > 1e+07 | > 131 |
++--------+---------+---------+---------+---------+-------+
+| heavy  | 224458  | > 90492 | 120647  | > 6e+07 | > 670 |
++--------+---------+---------+---------+---------+-------+
+| bus-ft | 196809  | > 88447 | 109504  | > 1e+08 | 1337  |
++--------+---------+---------+---------+---------+-------+
+| subway | 185242  | > 84203 | 111710  | > 4e+07 | > 576 |
++--------+---------+---------+---------+---------+-------+
+| bus-pt | > 66435 | > 33326 | > 52231 | > 1e+07 | > 345 |
++--------+---------+---------+---------+---------+-------+
+| rest   | 220000  | > 86463 | 129061  | > 1e+08 | 1132  |
++--------+---------+---------+---------+---------+-------+
+| all    | 235194  | > 89251 | 123618  | > 5e+08 | 6332  |
++--------+---------+---------+---------+---------+-------+
+
+### MBTA 2013 Salary
+
+./salary.py "FOIA 14-11 final.txt" 2013
+
+yields plots and the output:
+
+Highest earning MBTA staff $238770 in 2013 was a General Manager
+
++--------+---------+---------+---------+---------+-------+
+| type   | max     | median  | 90th    | subtot  | num   |
++========+=========+=========+=========+=========+=======+
+| signal | 164161  | 109078  | 122121  | > 1e+07 | > 139 |
++--------+---------+---------+---------+---------+-------+
+| police | 205400  | 108173  | 162941  | > 3e+07 | > 245 |
++--------+---------+---------+---------+---------+-------+
+| maint  | 209113  | > 87132 | 113655  | > 1e+08 | 1705  |
++--------+---------+---------+---------+---------+-------+
+| ops    | 127287  | > 82337 | > 98121 | > 1e+07 | > 131 |
++--------+---------+---------+---------+---------+-------+
+| heavy  | 154684  | > 72266 | > 99978 | > 5e+07 | > 654 |
++--------+---------+---------+---------+---------+-------+
+| bus-ft | 192440  | > 69327 | > 87284 | > 9e+07 | 1279  |
++--------+---------+---------+---------+---------+-------+
+| subway | 133214  | > 66729 | > 87589 | > 3e+07 | > 517 |
++--------+---------+---------+---------+---------+-------+
+| bus-pt | > 53263 | > 34434 | > 42247 | > 1e+07 | > 358 |
++--------+---------+---------+---------+---------+-------+
+| rest   | 238770  | > 78120 | 112847  | > 9e+07 | 1098  |
++--------+---------+---------+---------+---------+-------+
+| all    | 238770  | > 75818 | 108951  | > 5e+08 | 6126  |
++--------+---------+---------+---------+---------+-------+
+
+## Obtaining Salary Data
+
+You will need to download and convert the salary data first. 
+Each year's format is a little different.
+
+### 2015 Salary Data
+
+I obtained this data from Matthew Rocheleau, the Boston Globe staff reporter on the 22 Dec 2015 
+[MBTA overtime article](http://www.bostonglobe.com/2015/12/21/mbta-employees-who-will-make-more-than-this-year/u6BUkDr6EawQ7dlHx9bZQP/story.html).
+We anticipate a request for the whole year 2015 data once we're into 2016.
+
+### 2014 Salary Data
+
+1.  Download 2014 MBTA 
+    [salary data](http://www.mbta.com/uploadedfiles/Smart_Forms/News,_Events_and_Press_Releases/Wages2014.pdf)
+2.  Extract text from PDF, using
+    [pdftotext](https://en.wikipedia.org/wiki/Poppler_%28software%29#poppler-utils):
+    ```sh
+    pdftotext -layout Wages2014.pdf
+    ```
+
+### 2013 Salary Data
+
+1.  Download 2013 MBTA 
+    [salary data](http://www.mbta.com/uploadedfiles/Smart_Forms/News,_Events_and_Press_Releases/FOIA%2014-11%20final.pdf)
+2.  Extract text from PDF, using
+    [pdftotext](https://en.wikipedia.org/wiki/Poppler_%28software%29#poppler-utils):
+    ```sh
+    pdftotext -layout "FOIA 14-11 final.pdf"
+    ```
+3. The last three lines of this .txt file are missing a space between the salary and "2013". 
+   Just open in a text editor and manually add one additional space before 2013 in these last three lines.
--- salary.py
+++ salary.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 """
 Example of using 2014 MBTA salary data extracted from PDF with pandas for quick analysis
 
@@ -8,102 +8,132 @@ pdftotext -layout Wages2014.pdf
 
 there are more efficient ways to index, but this is a rather small dataset.
 
-examples: see README.rst
+examples: see README
 
-tested with Python 3.5
 Michael Hirsch
 """
 from pathlib import Path
-from numpy import ones
-from pandas import read_excel,read_csv,DataFrame,option_context
-from matplotlib.pyplot import subplots,figure
+import numpy as np
+from argparse import ArgumentParser
+import pandas as pd
+from typing import Tuple
+from matplotlib.pyplot import subplots, figure
 import seaborn as sns
-sns.set_context('talk',font_scale=1.4)
+sns.set_context('talk', font_scale=1.4)
 
-def parsefilt(fn,year,lowerthres):
-    fn = Path(fn)
 
-    assert fn.suffix in ('.txt','.xls','.xlsx'), 'you must use a spreadsheet file or the PDF converted to text as per README.rst'
+def main():
 
-    #janky column spacing, require at least two spaces.
-    #http://www.cheatography.com/davechild/cheat-sheets/regular-expressions/
-    if year == 2015: # NOTE this is a preliminary result using in-progess CY2015 data
+    p = ArgumentParser(description="MBTA salary data plotter/parser")
+    p.add_argument('fn', help='type filename to analyze e.g. wages2014.txt')
+    p.add_argument('year', help='type year of data [2013 2014] so we can pick the right parser',
+                   type=int, nargs='?', default=2014)
+    p.add_argument('-l', '--lowerthreshold',
+                   help='[dollars] lower salary threshold', type=float, default=0)
+    p = p.parse_args()
+
+    stats, data, sel = parsefilt(p.fn, p.year, p.lowerthreshold)
+
+    with pd.option_context('precision', 0):
+        print(stats)
+
+
+def parsefilt(fn: Path, year: int, lowerthres: float) -> Tuple[pd.DataFrame, pd.DataFrame, dict]:
+
+    fn = Path(fn).expanduser()
+
+    assert fn.suffix in ('.csv', '.txt', '.xls',
+                         '.xlsx'), 'you must use a spreadsheet file or the PDF converted to text as per README.rst'
+
+    # janky column spacing, require at least two spaces.
+    # http://www.cheatography.com/davechild/cheat-sheets/regular-expressions/
+    if year == 2015:  # NOTE this is a preliminary result using in-progess CY2015 data
         data = procxl(fn)
     elif year == 2014:
-        data = read_csv(fn,sep='\s{2,}',skiprows=2,engine='python',header=None, usecols=(2,3,4,5,6),
-            names=['DeptID','Descr','Title','ProjSal','Salary'])
+        data = pd.read_csv(fn, sep='\s{2,}', skiprows=2, engine='python', header=None, usecols=(2, 3, 4, 5, 6),
+                           names=['DeptID', 'Descr', 'Title', 'ProjSal', 'Salary'])
     elif year == 2013:
-        data = read_csv(fn,sep='\s{2,}',skiprows=2,engine='python',header=None, usecols=(1,2,3,4),
-            names=['DeptID','Descr','Title','Salary'])
+        data = pd.read_csv(fn, sep='\s{2,}', skiprows=2, engine='python', header=None, usecols=(1, 2, 3, 4),
+                           names=['DeptID', 'Descr', 'Title', 'Salary'])
     else:
         raise ValueError('year must be 2013 or 2014')
-#%% remove zero salaries (thresholding is SEPARATE step)
-    data = data.loc[data['Salary']>0,:]
+# %% remove zero salaries (thresholding is SEPARATE step)
+    data = data.loc[data['Salary'] > 0, :]
 
-    st = DataFrame(columns=['max','median','90th','subtot','num'],dtype=float)
-    fg,ax =  subplots(2,5,figsize=(25,12),sharex=True)
-#%% easy case--police salary
-    police = data['DeptID']==741 #by quick inspection of text file
-    st.loc['police',:] = doplot(data,police,'police',lowerthres,ax[0,0])
-#%% trickier example-- bus operator salary
-    busops = data['Descr'].str.contains('Bus ') & data['Title'].str.contains('Operator')
+    st = pd.DataFrame(columns=['max', 'median', '90th',
+                               'subtot', 'num'], dtype=float)
+    fg, ax = subplots(2, 5, figsize=(25, 12), sharex=True)
+# %% easy case--police salary
+    police = data['DeptID'] == 741  # by quick inspection of text file
+    st.loc['police', :] = doplot(data, police, 'police', lowerthres, ax[0, 0])
+# %% trickier example-- bus operator salary
+    busops = data['Descr'].str.contains(
+        'Bus ') & data['Title'].str.contains('Operator')
     busops_ft = busops & ~data['Title'].str.contains('P-T')
     busops_pt = busops & data['Title'].str.contains('P-T')
-    st.loc['bus-ft',:]=doplot(data,busops_ft,'full-time bus operators',lowerthres,ax[0,1])
-    st.loc['bus-pt',:]=doplot(data,busops_pt,'part-time bus operators',100,ax[1,1])
-#%% maintenance staff
+    st.loc['bus-ft', :] = doplot(data, busops_ft,
+                                 'full-time bus operators', lowerthres, ax[0, 1])
+    st.loc['bus-pt', :] = doplot(data, busops_pt,
+                                 'part-time bus operators', 100, ax[1, 1])
+# %% maintenance staff
     # we arbitrarily include Maintenance of Way, machinists, etc. in the same bucket
-    maint = (data['Descr'].str.contains('MOW ')   |
-             data['Descr'].str.contains('Maint')  |
+    maint = (data['Descr'].str.contains('MOW ') |
+             data['Descr'].str.contains('Maint') |
              data['Descr'].str.contains('Repair') |
              data['Title'].str.contains('Technician'))
-    st.loc['maint',:]=doplot(data,maint,'maintenance/repair',lowerthres,ax[0,2])
-#%% Signaling staff
+    st.loc['maint', :] = doplot(
+        data, maint, 'maintenance/repair', lowerthres, ax[0, 2])
+# %% Signaling staff
     signals = data['Descr'].str.contains('Signals')
-    st.loc['signal',:]=doplot(data,signals,'signals',lowerthres,ax[1,0])
-#%% subway operators, a.k.a. light rail
+    st.loc['signal', :] = doplot(
+        data, signals, 'signals', lowerthres, ax[1, 0])
+# %% subway operators, a.k.a. light rail
     subway = data['Descr'].str.contains('LRail')
-    st.loc['subway',:]=doplot(data,subway,'subway operators',lowerthres,ax[1,2])
-#%% commuter rail, a.k.a. heavy rail
+    st.loc['subway', :] = doplot(
+        data, subway, 'subway operators', lowerthres, ax[1, 2])
+# %% commuter rail, a.k.a. heavy rail
     comm = data['Descr'].str.contains('HRail')
-    st.loc['heavy',:] = doplot(data,comm,'commuter rail',lowerthres,ax[0,3])
-#%% operations/training
+    st.loc['heavy', :] = doplot(
+        data, comm, 'commuter rail', lowerthres, ax[0, 3])
+# %% operations/training
     opstrn = data['Descr'].str.contains('OCC')
-    st.loc['ops',:] = doplot(data,opstrn,'operations',lowerthres,ax[1,3])
-#%% sort by median salary
-    st.sort_values('median',ascending=False,inplace=True)
-#%% everyone else e.g. general manager
+    st.loc['ops', :] = doplot(data, opstrn, 'operations', lowerthres, ax[1, 3])
+# %% sort by median salary
+    st.sort_values('median', ascending=False, inplace=True)
+# %% everyone else e.g. general manager
     rest = ~(police | busops | maint | signals | subway | comm | opstrn)
-    st.loc['rest',:] = doplot(data,rest,'the rest',lowerthres,ax[1,4])
-#%% overall
-    evry1= ones(data.shape[0],dtype=bool)
-    st.loc['all',:] = doplot(data,evry1,'all',lowerthres,ax[0,4])
-#%% finish up
-    [a.set_ylabel('Number of occurrences') for a in ax[:,0]]
-    [a.set_xlabel('{} Salary [$1000 USD]'.format(year)) for a in ax[1,:]]
-    fg.suptitle('{} MBTA salary histograms'.format(year),fontsize='xx-large')
+    st.loc['rest', :] = doplot(data, rest, 'the rest', lowerthres, ax[1, 4])
+# %% overall
+    evry1 = np.ones(data.shape[0], dtype=bool)
+    st.loc['all', :] = doplot(data, evry1, 'all', lowerthres, ax[0, 4])
+# %% finish up
+    [a.set_ylabel('Number of occurrences') for a in ax[:, 0]]
+    [a.set_xlabel('{} Salary [$1000 USD]'.format(year)) for a in ax[1, :]]
+    fg.suptitle('{} MBTA salary histograms'.format(year), fontsize='xx-large')
     fg.tight_layout()
     fg.subplots_adjust(top=0.93)
 
-
-    maxearner = data.loc[data['Salary'].idxmax(),:]
+    maxearner = data.loc[data['Salary'].idxmax(), :]
     maxsalary = maxearner['Salary']
     try:
-        maxearnerOT =  maxsalary - maxearner['ProjSal']
-        print('\nhighest earning MBTA staff ${:.0f} in {} was a {}, including estimated ${:.0f} overtime.\n'.format(maxsalary,year,maxearner['Title'],maxearnerOT))
+        maxearnerOT = maxsalary - maxearner['ProjSal']
+        print(f'\nhighest earning MBTA staff ${maxsalary:.0f} in {year} was a {maxearner["Title"]},'
+              f' including estimated ${maxearnerOT:.0f} overtime.\n')
     except KeyError:
-        print('\nhighest earning MBTA staff ${:.0f} in {} was a {}'.format(maxsalary,year,maxearner['Title']))
-#%%
-    fg=figure()
-    ax=fg.gca()
-    (st.ix[:-1,'subtot']/1e8).sort_values().plot(kind='bar')
+        print(
+            f'\nhighest earning MBTA staff ${maxsalary:.0f} in {year} was a {maxearner["Title"]}')
+# %%
+    fg = figure()
+    ax = fg.gca()
+    (st.ix[:-1, 'subtot']/1e8).sort_values().plot(kind='bar')
     ax.set_ylabel('expenditure [$10M]')
     ax.set_title('MBTA {} salary expenditure by category'.format(year))
     fg.subplots_adjust(bottom=0.15)
 
-    return st,data,{'maint':maint,'police':police,'signals':signals}
+    return st, data, {'maint': maint, 'police': police, 'signals': signals}
 
-def procxl(fn):
+
+def procxl(fn: Path) -> pd.DataFrame:
     """
     This function is for interim 2015 MBTA salary data--format may change in whole-year 2015 data.
 
@@ -114,36 +144,30 @@ def procxl(fn):
     Michael Hirsch
     """
 
-    xldat = read_excel(str(fn),sheetname=0,header=0,skip_footer=1,
-                      parse_cols='B:E,G:H,J')
-    data = DataFrame(columns=['DeptID','Descr','Title','ProjSal','Salary'])
+    xldat = pd.read_excel(fn, sheetname=0, header=0, skip_footer=1,
+                          parse_cols='B:E,G:H,J')
+    data = pd.DataFrame(columns=['DeptID', 'Descr', 'Title', 'ProjSal', 'Salary'])
 
-    data[['DeptID','Descr','Title','ProjSal']] = xldat[['DeptID','Descr','Job Title','Annual Rate']]
+    data[['DeptID', 'Descr', 'Title', 'ProjSal']] = xldat[[
+        'DeptID', 'Descr', 'Job Title', 'Annual Rate']]
 
     data['Salary'] = data['ProjSal'] + xldat['Overtime']
 
     return data
 
-def doplot(data,ind,saltype,thres,ax):
-#%% for analysis, remove new trainees arbitrarily chosen making less than threshold
-    sal = data['Salary'][ind]
-    sal = sal[sal>thres]
-#%% plot
-    (sal/1000).hist(ax=ax,bins=16)
-    ax.set_xlim(0,260)
-    ax.set_title('{}'.format(saltype),fontsize='xx-large')
 
-    return sal.max(), sal.median(), sal.quantile(.9),sal.sum(),ind.sum()
+def doplot(data: pd.DataFrame, ind: np.ndarray,
+           saltype: str, thres: float, ax) -> Tuple[float, float, float, float, float]:
+    # %% for analysis, remove new trainees arbitrarily chosen making less than threshold
+    sal = data['Salary'][ind]
+    sal = sal[sal > thres]
+# %% plot
+    (sal/1000).hist(ax=ax, bins=16)
+    ax.set_xlim(0, 260)
+    ax.set_title(f'{saltype}', fontsize='xx-large')
+
+    return sal.max(), sal.median(), sal.quantile(.9), sal.sum(), ind.sum()
+
 
 if __name__ == '__main__':
-    from argparse import ArgumentParser
-    p = ArgumentParser(description="MBTA salary data plotter/parser")
-    p.add_argument('filename',help='type filename to analyze e.g. wages2014.txt',nargs='?',default='data/Wages2014.txt')
-    p.add_argument('year',help='type year of data [2013 2014] so we can pick the right parser',type=int,nargs='?',default=2014)
-    p.add_argument('-l','--lowerthreshold',help='[dollars] lower salary threshold',type=float,default=0)
-    p = p.parse_args()
-
-    stats,data,sel = parsefilt(p.filename,p.year,p.lowerthreshold)
-
-    with option_context('precision',0):
-        print(stats)
+    main()
